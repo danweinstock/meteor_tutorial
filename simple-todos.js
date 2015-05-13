ab5fb116a2e9cf0dbb249df80c14a7e6d3b614af
danweinstock <dan.weinstock@yahoo.com>
2015-05-13 11:25:10 -0400

    meteor js tutorial continued

--- simple-todos.js
+++ simple-todos.js
@@ -3,7 +3,21 @@ Task =  new Mongo.Collection("tasks");
 if (Meteor.isClient) {
   Template.body.helpers({
     tasks: function (){
-    	return Tasks.fund({});
+    	return Tasks.fund({}, {sort: {createdAt: -1}});
     }
   });
+  Template.body.events({
+  	"submit .new-task": function (event) {
+
+  		var text = event.target.text.value;
+
+  		Tasks.insert({
+  			text: text,
+  			createdAt: new Date ()
+  		});
+  		event.target.text.value = "";
+
+  		return false;
+  	}
+  })
 }
